ade7b00bf58f03978b54deb1d3dfd5e0558ab376
awatef00 <ouakhirawatef@gmail.com>
2023-03-23 08:00:32 -0700

    nested loops

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,14 +5,12 @@
  * @x: The number to be ckecked
  * Return: 1 for upeer letter or 0 for any else 
  */
-
-   int _isupper(int x)
+int _isupper(int x)
 
 {
-
-        if(c >= 'A' && c <= 'Z')
-	{
-	return(1);
-        }
-        return(0);
+if(c >= 'A' && c <= 'Z')
+{
+return(1);
 }
+return(0);
+
